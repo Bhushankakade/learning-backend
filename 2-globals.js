e80302914609fc537c98f3f156bf8206b5605227
Bhushan <kakadebhushan616@gmail.com>
2024-11-05 12:51:11 +0530

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,12 @@
+// Globals no window -
+// __dirname = path to current directory
+// __filename = file name
+// require = function to use module (commanjs)
+// module - info about current moule (file)
+// process - info about env where the program is being executed
+
+console.log(__dirname);
+
+setInterval(() => {
+  console.log("Hello world");
+}, 1000);
